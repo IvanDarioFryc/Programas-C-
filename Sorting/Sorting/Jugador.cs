ef57334142c06a1761cebe4131684369f454d3e7
IvanDarioFryc <111554063+IvanDarioFryc@users.noreply.github.com>
2022-08-19 02:17:35 -0300

    Add files via upload
    ejercicio de sorting donde ordenaremos una lista de objetos en base a sus atributos (Nombre, Puntuacion, Partidas Perdidas) y en base a ciertos criterios de forma descendente o ascendente.

---
+++ Sorting/Sorting/Jugador.cs
@@ -0,0 +1,19 @@
+﻿internal class Jugador
+{
+    public string nombre;
+    public int puntuacion;
+    public int partidasPerdidas;
+
+    public Jugador(string nombre, int puntuacion, int partidasPerdidas)
+    {
+        this.nombre = nombre;
+        this.puntuacion = puntuacion;
+        this.partidasPerdidas = partidasPerdidas;
+
+    }
+
+    public override string ToString()
+    {
+        return  nombre + "  " + puntuacion + "  " + partidasPerdidas  ;
+    }
+}
---
+++ Sorting/Sorting/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Example
+{
+    public static void Main()
+    {
+        //Metodos
+        void ordenarPorPuntuacionYNombre(List<Jugador> lista)
+        {
+            lista.Sort(delegate (Jugador x, Jugador y)
+            {
+
+                if (x.puntuacion == y.puntuacion)
+                {
+                    return x.nombre.CompareTo(y.nombre);
+                }
+                else
+                {
+                    return y.puntuacion.CompareTo(x.puntuacion);
+                }
+
+
+            });
+
+            Console.WriteLine(String.Join(Environment.NewLine, lista));
+        }
+        void ordenarPorPuntuacionPerdidasYNombre(List<Jugador> lista)
+        {
+            lista.Sort(delegate (Jugador x, Jugador y)
+            {
+
+                if (x.puntuacion == y.puntuacion)
+                {
+                    if (x.partidasPerdidas==y.partidasPerdidas)
+                    {
+                        return x.nombre.CompareTo(y.nombre);
+                    }
+                    else
+                    {
+                        return x.partidasPerdidas.CompareTo(y.partidasPerdidas);
+                    }
+                }
+                else
+                {
+                    return y.puntuacion.CompareTo(x.puntuacion);
+                }
+
+
+            });
+
+            Console.WriteLine(String.Join(Environment.NewLine, lista));
+        }
+
+        //declaramos los constructores
+        Jugador jug1 = new Jugador("Ivan", 10000, 10);
+        Jugador jug2 = new Jugador("Federico", 8000, 15);
+        Jugador jug3 = new Jugador("Sebastian", 8000, 15);
+        Jugador jug4 = new Jugador("Lucas", 6000, 20);
+        Jugador jug5 = new Jugador("Martin", 6000, 21);
+        Jugador jug6 = new Jugador("Alejandro", 6000, 23);
+        Jugador jug7 = new Jugador("Damian", 8000, 16);
+
+
+        //declaramos la lista
+        List<Jugador> ListaJugadores = new List<Jugador>() { jug1, jug2, jug3, jug4, jug5, jug6, jug7 };
+
+        //aplicamos los metodos para que nos devuelvan las listas ordenadas.
+
+        //El orden se rige por los puntos y la primer letra del nombre. La cantidad de partidas perdidas no influye.
+        Console.WriteLine("Lista ordenada por puntos de manera descendente y por orden alfabetico ascendente:\n");
+        Console.WriteLine("\nNombre   Puntos  Partidas Perdidas\n");
+        ordenarPorPuntuacionYNombre(ListaJugadores);
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        //El orden se rige primero por los puntos, luego por la cantidad de partidas perdidas, y despues po la primer letra del nombre. 
+        Console.WriteLine("Lista ordenada por puntos y partidas perdidas de manera descendente y por orden alfabetico ascendente:\n");
+        Console.WriteLine("\nNombre   Puntos  Partidas Perdidas\n");
+        ordenarPorPuntuacionPerdidasYNombre(ListaJugadores);
+
+
+        Console.ReadKey();
+    }
+}
