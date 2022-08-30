826420eb1603f107606f0d70a6bee8ffd5c2490a
Ivan Fryc <111554063+IvanDarioFryc@users.noreply.github.com>
2022-08-30 02:12:19 -0300

    Add files via upload
    ¡¡Calcula el factorial de un numero!!

---
+++ Factorial de un numero/Factorial de un numero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Example
+{
+    private static int calcularFactorial(int numero)
+    {
+        int factorial = 1;
+        for (int i = numero; i > 0; i--)
+        {
+            factorial=factorial*i;
+        }
+
+        return factorial;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Ingrese un numero: ");
+        string num = Console.ReadLine();
+        int numero = Int16.Parse(num);
+
+        Console.WriteLine("\nEl factorial de " + numero + " es:");
+        Console.WriteLine(calcularFactorial(numero));
+
+        Console.ReadKey();
+    }
+}
